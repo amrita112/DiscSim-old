2c911272e8bdf6fe3f628f9dac0700910942f29d
Amrita Singh <singha@janelia.hhmi.org>
2023-06-07 17:03:12 -0400

    Updated discrepancy fn with 5 methods to calculate score

--- disc_score.py
+++ disc_score.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 def discrepancy_score(subordinate_variable, supervisor_variable, method):
     """Calculates the discrepancy score between two variables."""
     """The discrepancy score is a measure of the difference between the two variables."""
@@ -11,5 +13,20 @@ def discrepancy_score(subordinate_variable, supervisor_variable, method):
         raise TypeError("The two variables must be the same type.")
     
     # Step 3: calculate the discrepancy score
-    if method == "mean":
-        
+    if method == "percent_difference":
+        discrepancy_score = np.mean(np.divide((subordinate_variable - supervisor_variable), supervisor_variable)) * 100
+    elif method == "absolute_difference":
+        discrepancy_score = np.mean(abs(subordinate_variable - supervisor_variable))
+    elif method == "absolute_percent_difference":
+        discrepancy_score = np.mean(abs((subordinate_variable - supervisor_variable) / supervisor_variable * 100))
+    elif method == "simple_difference":
+        discrepancy_score = np.mean(subordinate_variable - supervisor_variable)
+    elif method == "percent_non_match":
+        discrepancy_score = (subordinate_variable != supervisor_variable) / len(subordinate_variable) * 100
+    else:
+        raise ValueError("The method must be one of the following: percent_difference, absolute_difference, absolute_percent_difference, simple_difference, or percent_non_match.")
+    
+    return discrepancy_score
+
+
+
