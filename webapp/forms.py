39f2f019266ec341a25218e6ce963df89a642071
Amrita Singh <amsi.1994@gmail.com>
2023-09-12 11:27:46 -0400

    Added __init__.py
    To make /Scripts a package from which modules can be imported.

--- webapp/forms.py
+++ webapp/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, DecimalField
 from wtforms.validators import DataRequired
 
 class LoginForm(FlaskForm):
@@ -8,3 +8,8 @@ class LoginForm(FlaskForm):
     remember_me = BooleanField('Remember me')
     submit = SubmitField('Sign in')
     
+class SampleSizeForm(FlaskForm): 
+    threshold = DecimalField('Threshold', validators = [DataRequired()])
+    accuracy = DecimalField('Accuracy')
+    confidence = DecimalField('Confidence')
+    submit = SubmitField('Generate number of samples')
